4c2b750331f51698c84065b3077b8f4f9f5a9925
omermalik13 <omer.farooq812@gmail.com>
2020-10-08 14:07:57 -0400

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,13 +1,11 @@
-from django.urls import path
-
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-	
-	path('', views.store, name="store"),
-	path('cart/', views.cart, name="cart"),
-	path('checkout/', views.checkout, name="checkout"),
-
-	path('update_item/', views.updateItem, name="update_item"),
-	path('process_order/', views.processOrder, name="process_order"),
+    path('admin/', admin.site.urls),
+    path('', include('store.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
